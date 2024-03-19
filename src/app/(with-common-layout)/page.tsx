3a770652d00fbcac4addc1650486fdb845be55c3
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-20 05:52:59 +0600

    why us section

--- src/app/(with-common-layout)/page.tsx
+++ src/app/(with-common-layout)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/Ui/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/Ui/HomePage/Specialist/Specialist";
 import TopRatedDoctors from "@/components/Ui/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/Ui/HomePage/WhyUs/WhyUs";
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <HeroSection />
       <Specialist />
       <TopRatedDoctors />
+      <WhyUs />
     </>
   );
 };
