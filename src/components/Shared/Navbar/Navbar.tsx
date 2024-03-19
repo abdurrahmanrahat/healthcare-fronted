d20c5e09a95fa361d8acb0c8a245fc9107286d53
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-19 13:49:39 +0600

    navbar

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 
 const Navbar = () => {
   return (
     <Container>
-      <Stack py={2} direction="row" justifyContent="space-between">
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Typography variant="h5" component="h1" fontWeight={600}>
           P
           <Box component="span" color="primary.main">
@@ -11,6 +16,16 @@ const Navbar = () => {
           </Box>{" "}
           HealthCare
         </Typography>
+
+        <Stack direction="row" gap={4} justifyContent="space-between">
+          <Typography>Consultation</Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>NFOs</Typography>
+        </Stack>
+
+        <Button>Login</Button>
       </Stack>
     </Container>
   );
