e978b3120cf66289844302c65e81f4d43a47f7a2
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-19 19:19:43 +0600

    specialist section

--- src/app/(with-common-layout)/page.tsx
+++ src/app/(with-common-layout)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/Ui/HomePage/HeroSection/HeroSection";
+import Specialist from "@/components/Ui/HomePage/Specialist/Specialist";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Specialist />
     </>
   );
 };
---
+++ src/components/Ui/HomePage/Specialist/Specialist.tsx
@@ -0,0 +1,37 @@
+import { Box, Container, Typography } from "@mui/material";
+
+const Specialist = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/specialties", {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const specialists = await res.json();
+  console.log(specialists);
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          margin: "40px 0px",
+          textAlign: "center",
+        }}
+      >
+        <Box
+          sx={{
+            textAlign: "start",
+          }}
+        >
+          <Typography variant="h4" fontWeight={600}>
+            Explore Treatments Across Specialist
+          </Typography>
+          <Typography component="p" fontWeight={300} fontSize={18}>
+            Experienced Doctors Across All Specialists
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Specialist;
