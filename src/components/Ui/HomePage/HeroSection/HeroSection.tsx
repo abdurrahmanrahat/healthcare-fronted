7d7ede7b8ae504daaeb8564e29bdf1d9bfb9174f
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-19 15:02:26 +0600

    left side in hero sectioon

--- src/components/Ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/Ui/HomePage/HeroSection/HeroSection.tsx
@@ -1,5 +1,5 @@
 import assets from "@/assets";
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Image from "next/image";
 
 const HeroSection = () => {
@@ -11,6 +11,7 @@ const HeroSection = () => {
         my: 16,
       }}
     >
+      {/* left side */}
       <Box
         sx={{
           flex: 1,
@@ -27,7 +28,59 @@ const HeroSection = () => {
         >
           <Image src={assets.svgs.grid} alt="grid" />
         </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "16px",
+            justifyContent: "center",
+          }}
+        >
+          <Box>
+            <Typography variant="h3" component="h1" fontWeight={600}>
+              Healthier Hearts
+            </Typography>
+            <Typography variant="h3" component="h1" fontWeight={600}>
+              Come From
+            </Typography>
+            <Typography
+              color="primary.main"
+              variant="h3"
+              component="h1"
+              fontWeight={600}
+            >
+              Preventive Care
+            </Typography>
+          </Box>
+
+          <Box>
+            <Typography
+              //   variant="h6"
+              component="p"
+              fontWeight={400}
+              className="max-w-[60ch] text-[14px]"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos ab magnam quasi vero officia, amet laboriosam corrupti
+              quisquam minima beatae neque non accusantium dolor explicabo
+              labore.
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              gap: "8px",
+            }}
+          >
+            <Button>Make Appointment</Button>
+            <Button variant="outlined">Contact Us</Button>
+          </Box>
+        </Box>
       </Box>
+
+      {/* right side */}
       <Box>right</Box>
     </Container>
   );
