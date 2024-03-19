2e278440f45f7c20988c161fde23af367a5a384e
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-19 19:38:47 +0600

    data dynamically add with card

--- src/components/Ui/HomePage/Specialist/Specialist.tsx
+++ src/components/Ui/HomePage/Specialist/Specialist.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
 const Specialist = async () => {
   const res = await fetch("http://localhost:5000/api/v1/specialties", {
@@ -6,8 +7,8 @@ const Specialist = async () => {
       revalidate: 30,
     },
   });
-  const specialists = await res.json();
-  console.log(specialists);
+  const { data: specialists } = await res.json();
+  //   console.log(specialists);
 
   return (
     <Container>
@@ -28,6 +29,50 @@ const Specialist = async () => {
           <Typography component="p" fontWeight={300} fontSize={18}>
             Experienced Doctors Across All Specialists
           </Typography>
+        </Box>
+
+        {/* display data in card */}
+        <Stack direction="row" gap={4} mt={5}>
+          {specialists?.map((item) => (
+            <Box
+              key={item.id}
+              sx={{
+                flex: 1,
+                width: "150px",
+                backgroundColor: "rgba(245, 245, 245, 1)",
+                border: "1px solid rgba(250, 250, 250, 1)",
+                borderRadius: "10px",
+                textAlign: "center",
+                padding: "40px 10px",
+                "& img": {
+                  width: "50px",
+                  height: "50px",
+                  margin: "0 auto",
+                },
+                "&:hover": {
+                  border: "1px solid blue",
+                  padding: "40px 10px",
+                  borderRadius: "10px",
+                },
+              }}
+            >
+              <Image
+                src={item.icon}
+                width={100}
+                height={100}
+                alt={item.title}
+              />
+              <Box>
+                <Typography component="p" fontWeight={600} fontSize={18} mt={2}>
+                  {item.title}
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+
+        <Box mt={5}>
+          <Button variant="outlined">View All</Button>
         </Box>
       </Box>
     </Container>
