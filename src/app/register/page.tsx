5dd9b6682909260918b552cae3762a37b4d3f784
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-26 22:39:18 +0600

    register page layout

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,47 @@
+import assets from "@/assets";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Register Here</h1>
-    </div>
+    <Container
+      sx={{
+        padding: "50px",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            padding: 4,
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
