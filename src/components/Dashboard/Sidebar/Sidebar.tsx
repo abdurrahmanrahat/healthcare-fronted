994f5f7838eebb91d1d4cecfa75d4e4c194b5b5d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-01 05:34:38 +0600

    TDrawerItems type

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -3,7 +3,6 @@ import MailIcon from "@mui/icons-material/Mail";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import {
   Box,
-  Divider,
   List,
   ListItem,
   ListItemButton,
@@ -20,19 +19,6 @@ const Sidebar = () => {
     <div>
       <List>
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,6 @@
 import { USER_ROLE } from "@/constants/tole";
+import { SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
 export type TMeta = {
   page: number;
@@ -8,3 +10,11 @@ export type TMeta = {
 
 // here, keyof take the all key of the obj and typeof take the all type of its value.
 export type TUserRole = keyof typeof USER_ROLE;
+
+export type TDrawerItems = {
+  title: string;
+  path: string;
+  parentPath?: string;
+  icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string };
+  child?: TDrawerItems[];
+};
