7663638f0956c95f926b35ffe3bd095f3ef1cdbd
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-01 05:26:17 +0600

    user role define

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,10 @@
+import { USER_ROLE } from "@/constants/tole";
+
 export type TMeta = {
   page: number;
   limit: number;
   total: number;
 };
+
+// here, keyof take the all key of the obj and typeof take the all type of its value.
+export type TUserRole = keyof typeof USER_ROLE;
