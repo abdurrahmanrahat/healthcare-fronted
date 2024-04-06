0668b8be6f001dd1ec228720f57a5924b66951ce
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-06 13:10:48 +0600

    table show

--- src/app/(with-dashboard-layout)/dashboard/admin/specialties/page.tsx
+++ src/app/(with-dashboard-layout)/dashboard/admin/specialties/page.tsx
@@ -2,6 +2,7 @@
 
 import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
 import { Box, Button, Stack, TextField } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useState } from "react";
 import SpecialtyModal from "./components/SpecialtyModal";
 
@@ -9,6 +10,15 @@ const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const { data, isLoading } = useGetAllSpecialtiesQuery({});
+
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 100 },
+    {
+      field: "icon",
+      headerName: "Icon",
+      width: 100,
+    },
+  ];
 
   return (
     <Box>
@@ -20,7 +30,13 @@ const SpecialtiesPage = () => {
       </Stack>
 
       {/* display specialties with table */}
-      <Box>Display</Box>
+      {!isLoading ? (
+        <Box>
+          <DataGrid rows={data} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </Box>
   );
 };
