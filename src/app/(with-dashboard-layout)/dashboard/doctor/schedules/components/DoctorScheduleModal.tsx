f66f4f035de8f756d7b369d300c1e302d8c1e32b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-26 20:13:55 +0600

    MultipleSelectField

--- src/app/(with-dashboard-layout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(with-dashboard-layout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,9 +1,11 @@
 import PHModal from "@/components/Shared/PHModal/PHModal";
+import { useGetAllSchedulesQuery } from "@/redux/api/scheduleApi";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import dayjs from "dayjs";
 import React, { useState } from "react";
+import MultipleSelectField from "./MultipleSelectField";
 
 type TModalProps = {
   open: boolean;
@@ -14,8 +16,28 @@ const DoctorScheduleModal = ({ open, setOpen }: TModalProps) => {
   const [selectedDate, setSelectedDate] = useState(
     dayjs(new Date()).toISOString()
   );
+  const [selectedScheduleIds, setSelectedScheduleIds] = useState();
 
-  console.log(selectedDate);
+  const query: Record<string, any> = {};
+
+  if (!!selectedDate) {
+    query["startDate"] = dayjs(selectedDate)
+      .hour(0)
+      .minute(0)
+      .millisecond(0)
+      .toISOString();
+
+    query["endDate"] = dayjs(selectedDate)
+      .hour(23)
+      .minute(59)
+      .millisecond(999)
+      .toISOString();
+  }
+
+  const { data } = useGetAllSchedulesQuery(query);
+  console.log(data);
+  const schedules = data?.schedules;
+  console.log(schedules);
 
   return (
     <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
@@ -28,6 +50,8 @@ const DoctorScheduleModal = ({ open, setOpen }: TModalProps) => {
           }
         />
       </LocalizationProvider>
+
+      <MultipleSelectField schedules={schedules} />
     </PHModal>
   );
 };
