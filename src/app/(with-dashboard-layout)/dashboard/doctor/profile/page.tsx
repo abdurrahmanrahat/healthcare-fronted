8fd51bd0f01688a5cc278686fd4e6ed7ee56785c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-27 18:05:57 +0600

    doctor information

--- src/app/(with-dashboard-layout)/dashboard/doctor/profile/page.tsx
+++ src/app/(with-dashboard-layout)/dashboard/doctor/profile/page.tsx
@@ -1,16 +1,89 @@
 "use client";
 
-import { useGetMYProfileQuery } from "@/redux/api/myProfile";
+import AutoFileUploader from "@/components/Forms/AutoFileUploader";
+import {
+  useGetMYProfileQuery,
+  useUpdateMYProfileMutation,
+} from "@/redux/api/myProfile";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { Box, Container } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Image from "next/image";
+// import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DoctorInformation from "./components/DoctorInformation";
 
-const DoctorProfile = () => {
-  const { data, isLoading } = useGetMYProfileQuery({});
-  console.log(data);
+const Profile = () => {
+  //   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { data, isLoading } = useGetMYProfileQuery(undefined);
+  const [updateMYProfile, { isLoading: updating }] =
+    useUpdateMYProfileMutation();
+
+  const fileUploadHandler = (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("data", JSON.stringify({}));
+
+    updateMYProfile(formData);
+  };
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    <p>Loading...</p>;
   }
 
-  return <div>DoctorProfile</div>;
+  return (
+    <>
+      {/* <ProfileUpdateModal
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+        id={data?.id}
+      /> */}
+      <Container>
+        <Grid container spacing={2}>
+          <Grid xs={12} md={4}>
+            <Box
+              sx={{
+                height: 300,
+                width: "100%",
+                overflow: "hidden",
+                borderRadius: 1,
+              }}
+            >
+              <Image
+                height={300}
+                width={400}
+                src={data?.profilePhoto}
+                alt="User Photo"
+              />
+            </Box>
+
+            {updating ? (
+              <p>Uploading...</p>
+            ) : (
+              <AutoFileUploader
+                name="file"
+                label="Choose Your Profile Photo"
+                icon={<CloudUploadIcon />}
+                onFileUpload={fileUploadHandler}
+                variant="text"
+              />
+            )}
+
+            {/* <Button
+              fullWidth
+              endIcon={<ModeEditIcon />}
+              onClick={() => setIsModalOpen(true)}
+            >
+              Edit Profile
+            </Button> */}
+          </Grid>
+          <Grid xs={12} md={8}>
+            <DoctorInformation data={data} />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
-export default DoctorProfile;
+export default Profile;
